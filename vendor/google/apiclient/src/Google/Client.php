c3b74cee581b603569118caac2a1182fccc1e620
Gary Constable <garyconstable80@gmail.com>
2016-01-12 18:38:07 +0000

    Security

--- vendor/google/apiclient/src/Google/Client.php
+++ vendor/google/apiclient/src/Google/Client.php
@@ -17,7 +17,6 @@
 
 require_once 'Google/Auth/AssertionCredentials.php';
 require_once 'Google/Cache/File.php';
-require_once 'Google/Cache/Memcache.php';
 require_once 'Google/Config.php';
 require_once 'Google/Collection.php';
 require_once 'Google/Exception.php';
